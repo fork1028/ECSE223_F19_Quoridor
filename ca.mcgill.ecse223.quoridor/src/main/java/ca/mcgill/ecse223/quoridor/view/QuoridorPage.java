e24611b6f3e55cf79260ae6f7f9168ed507058f2
fork1028 <xichen_@live.cn>
2019-10-19 23:29:15 -0400

    create view package

---
+++ ca.mcgill.ecse223.quoridor/src/main/java/ca/mcgill/ecse223/quoridor/view/QuoridorPage.java
@@ -0,0 +1,31 @@
+package ca.mcgill.ecse223.quoridor.view;
+
+import javax.swing.JFrame;
+
+public class QuoridorPage extends JFrame{
+
+	// UI elements
+	
+	
+	
+	/** Creates new QuoridorPage */
+	public QuoridorPage() {
+		initComponents();
+		refreshData();
+	}
+	
+	
+	/** This method is called from within the constructor to initialize the form.
+	 */
+	private void initComponents() {
+		
+		
+		// add listeners;
+	}
+	
+	private void refreshData() {
+		
+		
+	}
+	
+}
