abc6e8fe787c65a3d9244228dba7f702744ca418
fork1028 <xichen_@live.cn>
2019-10-29 14:43:07 -0400

    update stepDefinitions

--- ca.mcgill.ecse223.quoridor/src/main/java/ca/mcgill/ecse223/quoridor/view/QuoridorPage.java
+++ ca.mcgill.ecse223.quoridor/src/main/java/ca/mcgill/ecse223/quoridor/view/QuoridorPage.java
@@ -1,10 +1,32 @@
 package ca.mcgill.ecse223.quoridor.view;
 
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Dimension;
+import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.Properties;
+
+import javax.swing.GroupLayout;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JScrollPane;
+import javax.swing.JSeparator;
+import javax.swing.JTable;
+import javax.swing.JTextField;
+import javax.swing.ScrollPaneConstants;
+import javax.swing.SwingConstants;
+import javax.swing.WindowConstants;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableCellRenderer;
+
 
 public class QuoridorPage extends JFrame{
 
-	// UI elements
+	
+	
 	
 	
 	
@@ -18,6 +40,9 @@ public class QuoridorPage extends JFrame{
 	/** This method is called from within the constructor to initialize the form.
 	 */
 	private void initComponents() {
+		
+		
+		
 		
 		
 		// add listeners;
