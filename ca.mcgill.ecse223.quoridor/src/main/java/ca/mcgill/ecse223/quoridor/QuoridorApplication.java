6dee8e7550fa4c48e6e42a911257b5ebd9e54be2
Shayne Leitman <sleitman17@gmail.com>
2019-11-02 18:14:44 -0400

    Added the first iteration of the starting a game UI

--- ca.mcgill.ecse223.quoridor/src/main/java/ca/mcgill/ecse223/quoridor/QuoridorApplication.java
+++ ca.mcgill.ecse223.quoridor/src/main/java/ca/mcgill/ecse223/quoridor/QuoridorApplication.java
@@ -1,10 +1,26 @@
 package ca.mcgill.ecse223.quoridor;
 
 import ca.mcgill.ecse223.quoridor.model.Quoridor;
+import ca.mcgill.ecse223.quoridor.view.QuoridorStartGame;
 
 public class QuoridorApplication {
 
 	private static Quoridor quoridor;
+	
+	public static void main(String[] args) {
+		
+		java.awt.EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				new QuoridorStartGame().setVisible(true);
+			}
+			
+		});
+		
+		
+	}
+	
+	
+	
 
 	public static Quoridor getQuoridor() {
 		if (quoridor == null) {
--- ca.mcgill.ecse223.quoridor/src/main/java/ca/mcgill/ecse223/quoridor/view/QuoridorStartGame.java
+++ ca.mcgill.ecse223.quoridor/src/main/java/ca/mcgill/ecse223/quoridor/view/QuoridorStartGame.java
@@ -8,13 +8,14 @@ import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JSeparator;
 import javax.swing.JTextField;
 import javax.swing.WindowConstants;
 
-public class QuoridorPage extends JFrame{
-
-	private static final long serialVersionUID = -45345345345345345L;
-	//ALL OF THE FOLLOWING HAS BEEN COPY PASTED INTO QUORIDORSTARTGAME!!!!!!!!!
+public class QuoridorStartGame extends JFrame {
+	
+	private static final long serialVersionUID = -453453453453245345L;
+	
 	// UI elements
 	private JLabel errorMsg;
 	//Game
@@ -22,14 +23,18 @@ public class QuoridorPage extends JFrame{
 	private JButton loadGameButton;
 	private JTextField loadGameTextField;
 	private JButton resumeGameButton;
+	private JLabel loadGameLabel;
 	//User
-	private JComboBox<String> userList;
+	private JComboBox<String> whiteUserList;
+	private JComboBox<String> blackUserList;
 	private JTextField createUserTextField;
+	private JButton createUserButton;
 	private JLabel createUserLabel;
 	private JLabel blackPlayerLabel;
 	private JLabel whitePlayerLabel;
 	private JButton readyButton;
 	//Timer
+	private JLabel timerLabel;
 	private JComboBox<String> minuteList;
 	private JComboBox<String> secondList;
 	private JLabel setMinuteLabel;
@@ -41,7 +46,7 @@ public class QuoridorPage extends JFrame{
 	private HashMap<Integer, String> availableUser;
 	
 	/** Creates new QuoridorPage */
-	public QuoridorPage() {
+	public QuoridorStartGame() {
 		initComponents();
 		refreshData();
 	}
@@ -59,10 +64,15 @@ public class QuoridorPage extends JFrame{
 		loadGameButton = new JButton();
 		loadGameButton.setText("Load a Game:");
 		loadGameTextField = new JTextField();
+		loadGameLabel = new JLabel();
+		loadGameLabel.setText("Load Game File Name:");
 		resumeGameButton = new JButton();
 		resumeGameButton.setText("Resume Game");
 		//elements for User
-		userList = new JComboBox<String>(new String[0]);
+		createUserButton = new JButton();
+		createUserButton.setText("Create User");
+		whiteUserList = new JComboBox<String>(new String[0]);
+		blackUserList = new JComboBox<String>(new String[0]);
 		createUserTextField = new JTextField();
 		createUserLabel = new JLabel();
 		createUserLabel.setText("Create New User:");
@@ -74,6 +84,8 @@ public class QuoridorPage extends JFrame{
 		//maybe useless button??
 		readyButton.setText("Ready (USELESS BUTTON MAYBE????)");
 		//elements for Timer
+		timerLabel = new JLabel();
+		timerLabel.setText("Set Time");
 		minuteList = new JComboBox<String>(new String[0]);
 		secondList = new JComboBox<String>(new String[0]);
 		setMinuteLabel = new JLabel();
@@ -105,21 +117,87 @@ public class QuoridorPage extends JFrame{
 			}
 		});
 		
+		JSeparator horizontalLineMid = new JSeparator();
+		
 		//Layout
+		
+		//Horizontal Layout first.
 		GroupLayout layout = new GroupLayout(getContentPane());
 		getContentPane().setLayout(layout);
 		layout.setAutoCreateGaps(true);
 		layout.setAutoCreateContainerGaps(true);
-		//layout.setHorizontalGroup(
+		layout.setHorizontalGroup(
+				layout.createSequentialGroup()
+				.addGroup(layout.createParallelGroup()
+						.addComponent(errorMsg)
+						.addComponent(horizontalLineMid)
+						.addGroup(layout.createSequentialGroup()
+								.addGroup(layout.createParallelGroup()
+										.addComponent(createUserLabel)
+										.addComponent(whitePlayerLabel)
+										.addComponent(blackPlayerLabel)
+										.addComponent(timerLabel)
+										.addComponent(createNewGameButton)
+										.addComponent(loadGameLabel))
+								.addGroup(layout.createParallelGroup()
+										.addComponent(createUserTextField)
+										.addComponent(whiteUserList)
+										.addComponent(blackUserList)
+										.addComponent(setMinuteLabel)
+										.addComponent(minuteList)								
+										.addComponent(loadGameTextField))
+								.addGroup(layout.createParallelGroup()
+										.addComponent(createUserButton)
+										.addComponent(setSecondLabel)
+										.addComponent(secondList)
+										.addComponent(loadGameButton))
+						)
+				)
 				
-				//);
+		);
 		
+		//Vertical Layout now.
+		layout.setVerticalGroup(
+				layout.createParallelGroup()
+				.addGroup(layout.createSequentialGroup()
+						.addComponent(errorMsg)
+						.addGroup(layout.createParallelGroup()
+								.addComponent(createUserLabel)
+								.addComponent(createUserTextField)						
+								.addComponent(createUserButton))
+						.addGroup(layout.createParallelGroup()
+								.addComponent(whitePlayerLabel)
+								.addComponent(whiteUserList))
+						.addGroup(layout.createParallelGroup()
+								.addComponent(blackPlayerLabel)
+								.addComponent(blackUserList))					
+						.addGroup(layout.createParallelGroup()
+								.addComponent(setMinuteLabel)
+								.addComponent(setSecondLabel))	
+						.addGroup(layout.createParallelGroup()
+								.addComponent(timerLabel)
+								.addComponent(minuteList)						
+								.addComponent(secondList))
+						.addGroup(layout.createParallelGroup()
+								.addComponent(createNewGameButton))
+						.addGroup(layout.createParallelGroup()
+								.addComponent(horizontalLineMid))
+						.addGroup(layout.createParallelGroup()
+								.addComponent(loadGameLabel)
+								.addComponent(loadGameTextField)						
+								.addComponent(loadGameButton))
+				)
+		);
 		
+		pack();
 	}
 	
 	private void refreshData() {
 		
 		
 	}
+
+	
+	
 	
 }
