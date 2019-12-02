d27944a7f2eccb25f82bab1e9f736c2a07d6f46f
Sami <sami.kahil@mail.mcgill.ca>
2019-12-01 20:42:12 -0500

    almost done check path

--- ca.mcgill.ecse223.quoridor/src/main/java/ca/mcgill/ecse223/quoridor/controller/Graph.java
+++ ca.mcgill.ecse223.quoridor/src/main/java/ca/mcgill/ecse223/quoridor/controller/Graph.java
@@ -4,6 +4,7 @@ import java.io.*;
 import java.util.*;
 
 import ca.mcgill.ecse223.quoridor.model.Direction;
+import ca.mcgill.ecse223.quoridor.QuoridorApplication;
 import ca.mcgill.ecse223.quoridor.controller.QuoridorController;
 
 class Edge {
@@ -64,46 +65,57 @@ public class Graph {
 		}
 	}
 
-	public boolean wallAdded(int row, int col, Direction direction) {
+	public int wallAdded(int row, int col, Direction direction) {
 
 		if (direction.equals(Direction.Horizontal)) {
 			int node_1 = (row - 1) * 9 + col;
 			int node_2 = row * 9 + col;
 			int node_3 = (row - 1) * 9 + col + 1;
 			int node_4 = row * 9 + col + 1;
+			Edge edgeToDelete1 = null, edgeToDelete2 = null;
+
 			for (Edge e : edges) {
 				if (e.node_1 == node_1 && e.node_2 == node_2) {
-					edges.remove(e);
+					edgeToDelete1 = e;
 				}
 				if (e.node_1 == node_3 && e.node_2 == node_4) {
-					edges.remove(e);
+					edgeToDelete2 = e;
 				}
 			}
-
+			edges.remove(edgeToDelete1);
+			edges.remove(edgeToDelete2);
 		}
 		else {
 			int node_1 = (row - 1) * 9 + col;
 			int node_2 = (row - 1) * 9 + col + 1;
 			int node_3 = row * 9 + col;
 			int node_4 = row * 9 + col + 1;
+			Edge edgeToDelete1 = null, edgeToDelete2 = null;
+
 			for (Edge e : edges) {
 				if (e.node_1 == node_1 && e.node_2 == node_2) {
-					edges.remove(e);
+					edgeToDelete1 = e;
 				}
 				if (e.node_1 == node_3 && e.node_2 == node_4) {
-					edges.remove(e);
+					edgeToDelete2 = e;
 				}
 			}
+			edges.remove(edgeToDelete1);
+			edges.remove(edgeToDelete2);
 		}
 
 		return dfsSearch();
 	}
 
-	private boolean dfsSearch() {
+	// returns 00 if both white and black have no path
+	// returns 11 if both white and black have path
+	// returns 10 if white has path but black doesn't
+	// return 01 if black has path but white doesn't
+	private int dfsSearch() {
 		// white pawn
 		int whitePawnRow = QuoridorController.getCurrentRowForPawn(false);
 		int whitePawnCol = QuoridorController.getCurrentColForPawn(false);
-		
+
 		// black pawn
 		int blackPawnRow = QuoridorController.getCurrentRowForPawn(true);
 		int blackPawnCol = QuoridorController.getCurrentColForPawn(true);
@@ -111,10 +123,74 @@ public class Graph {
 		int whiteCoordinate = (whitePawnRow - 1) * 9 + whitePawnCol;
 		int blackCoordinate = (blackPawnRow - 1) * 9 + blackPawnCol;
 
+		boolean whitePath = dfs(whiteCoordinate, true);
+		boolean blackPath = dfs(blackCoordinate, false);
+		
+		if (whitePath == false && blackPath == false)
+			return 00;
+		
+		if (whitePath == true && blackPath == true)
+			return 11;
+		
+		if (whitePath == true && blackPath == false)
+			return 10;
+		
+		//if (whitePath == false && blackPath == true)
+		return 01;
+	}
+
+	private boolean dfs(int coordinate, boolean isWhitePlayer) {
+		// Initially mark all vertices as not visited 
+		Vector<Boolean> visited = new Vector<Boolean>(nodes.size()); 
+		for (int i = 0; i < nodes.size(); i++) 
+			visited.add(false); 
+
+		// Create a stack for DFS 
+		Stack<Integer> stack = new Stack<>(); 
+
+		// Push the current source node 
+		stack.push(coordinate); 
+
+		while(stack.empty() == false) { 
+			// Pop a vertex from stack and print it 
+			coordinate = stack.peek(); 
+			stack.pop(); 
+
+			// Stack may contain same vertex twice. So 
+			// we need to print the popped item only 
+			// if it is not visited. 
+			if(visited.get(coordinate) == false)   
+				visited.set(coordinate, true); 
+
+			// Get all adjacent vertices of the popped vertex s 
+			// If a adjacent has not been visited, then push it 
+			// to the stack. 
+
+			Iterator<Edge> itr = edges.iterator();
+
+			while (itr.hasNext()) { 
+				Edge v = itr.next(); 
+				if(!visited.get(v.node_1)) 
+					stack.push(v.node_1); 
+			} 
+		}
+		
+		if (isWhitePlayer) {
+			for (int i = 72; i <= 80; i++) {
+				if (visited.get(i) == false)
+					return false;
+			}
+		}
+		else {
+			for (int i = 1; i <= 8; i++) {
+				if (visited.get(i) == false)
+					return false;
+			}
+		}
 
 		return true;
 	}
-
+	
 	public void add_node(int n) {
 		if (!nodes.contains(n)) nodes.add(n);
 	}
